3214de1446501662ddaa5516505b74a34349d274
Aziza <aziza.saveleva@gmail.com>
2023-12-28 22:54:03 +0400

    Finaltask2

--- HomeWork10/Task10.py
+++ HomeWork10/Task10.py
@@ -4,6 +4,11 @@ lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
-pd.get_dummies(data['whoAmI'])
-# print(data.head()) 
-print(pd.get_dummies(data['whoAmI']))
+data.head()
+print(f"{data} \n")
+unique_values = data['whoAmI'].unique()
+for value in unique_values:
+    data[value] = (data['whoAmI'] == value).astype(int)
+data.drop('whoAmI', axis=1, inplace=True)
+data.head()
+print(data)
